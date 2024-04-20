7d821fa0cbaec6c9590fd82d3c39a2da29c69c18
BigPeanutFromStudio <bielanskipiotr8@gmail.com>
2024-04-20 09:47:22 +0200

    Added a handler for creating user

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/BigPeanutFromStudio/bingo/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct{
-	//DB *database.Queries
+	DB *database.Queries
 }
 
 func main() {
@@ -22,6 +26,21 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == ""{
 		log.Fatal("PORT variable not found in environment")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == ""{
+		log.Fatal("DB_URL variable not found in environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+
+	if err != nil{
+		log.Fatal("error connecting to database: ", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -42,6 +61,7 @@ func main() {
 
 	//Handlers
 	v1Router.Post("/generate", generateBoard)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -59,7 +79,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil{
 		log.Fatal(err)
 	}
